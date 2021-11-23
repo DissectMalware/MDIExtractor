c6f90a1b3017f658c8d08eacefa92bbc1df1d075
Malwrologist <9646319+DissectMalware@users.noreply.github.com>
2021-11-23 09:25:28 +0000

    ADD - dridex extractor

---
+++ dridex_extractor.py
@@ -0,0 +1,50 @@
+import json
+
+from XLMMacroDeobfuscator.deobfuscator import process_file
+from XLMMacroDeobfuscator.configs.settings import SILENT
+import sys
+import os
+import base64
+import re
+
+dridex_file = sys.argv[1]
+
+result = process_file(file=dridex_file,
+            noninteractive= True,
+            noindent= True,
+            extract_formula_format='[[CELL-ADDR]]',
+            return_deobfuscated= True,
+            extract_only= True,
+            sort_formulas = True,
+            silent= True,
+            timeout= 30)
+
+if len(result) > 1:
+    sheet = result[0].replace('SHEET:','').split(',')[0].strip()
+    start_addr = "'{}'!{}".format(sheet, result[1])
+
+    print("[Start Point]: {}".format(start_addr))
+
+    json_output_path = dridex_file+'.json'
+    result = process_file(file=dridex_file,
+                          noninteractive=True,
+                          noindent=True,
+                          output_formula_format='[[CELL-ADDR]], [[INT-FORMULA]]',
+                          return_deobfuscated=True,
+                          export_json=json_output_path,
+                          start_point=start_addr,
+                          silent=True,
+                          timeout=30)
+
+    with open(json_output_path, 'r') as json_output:
+        result = json.load(json_output)
+        file_content = base64.b64decode(result['files'][0]['content_base64']).decode('utf_8')
+        regex_array = 'Array\((.*)\)'
+        array = re.compile(regex_array)
+        match = array.search(file_content)
+        if(match):
+            encoded_urls = match.group(1)
+            python_code = encoded_urls.replace('Chr', 'chr').replace('&', '+')
+            urls = eval(python_code)
+            print('\r\n'.join(urls))
+
